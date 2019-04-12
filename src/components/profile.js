6cabcb7b4432b3b64839726aa46e74029163e511
MaryCerasa <cerasame@gmail.com>
2019-04-12 10:31:42 +0100

    added all seeds, users, blog, comments front end profile page, and about page

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Search from '../components/search'
 
 class Profile extends React.Component {
   constructor() {
@@ -11,11 +12,29 @@ class Profile extends React.Component {
 
   render() {
     return (
-      <main>
-        <div className="profileForm">Profile
-        </div>
-
-      </main>
+      <div className="wrapper">
+        <main>
+          <div className="leftSide">
+            <div className="searchBar">
+              <Search />
+            </div>
+            <div className="aboutMe">
+              <h1>About Me</h1>
+            </div>
+          </div>
+          <div className="myBlogPosts">
+            <h1>My Blog Posts</h1>
+          </div>
+          <div className="rightSide">
+            <div className="faveBlogs">
+              <h1>Favorite Blogs</h1>
+            </div>
+            <div className="mySocial">
+              <h1>My Social Media</h1>
+            </div>
+          </div>
+        </main>
+      </div>
     )
   }
 
