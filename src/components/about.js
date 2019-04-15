ef0b4b738b60ce9bbea0947ce20a2e082b30e291
MaryCerasa <cerasame@gmail.com>
2019-04-15 14:33:17 +0100

    changes again

--- src/components/about.js
+++ src/components/about.js
@@ -20,7 +20,7 @@ class About extends React.Component {
             <br/>
             <p><strong>Wellnest</strong> is a platform created to provide a safe space for fostering mental health awareness and understanding of depression and other disorders. Our goal is to help you find your wings by initiating and maintaining a positive dialogue among peer support networks. This effort hopes to encourage communication surrounding mental health and wellness in order to break the stigma.</p>
             <br/>
-            <p>Guests can stay up to date with relevant news provided by the NHS or write posts about their experiences and recieve positive encouragement and support from their peers as well as seek advice from practicing therapists and mental health professionals through verified accounts.</p>
+            <p>Guests can stay up to date with relevant news provided by the NHS or write posts about their experiences and recieve positive encouragement and support from their peers as well as seek advice from practicing therapists and mental health professionals through verified accounts..</p>
             <br/>
           </div>
         </main>
