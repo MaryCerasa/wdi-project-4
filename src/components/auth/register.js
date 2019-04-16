b4f542417174d12d5c495eea4acef08f6e6c5515
Buse Uslu <buseuslu24@gmail.com>
2019-04-16 20:36:17 +0100

    register css

--- src/components/auth/register.js
+++ src/components/auth/register.js
@@ -50,51 +50,51 @@ class Register extends React.Component {
             <div className="registerForm">
               <h2>Welcome to WellNest!</h2>
               <h3>To sign up, please fill in the details below.</h3>
+              <div className="input">
+                <div className="username"><i className="fas fa-user"></i> &nbsp; Username:
+                </div>
 
-              <div className="username"><i className="fas fa-user"></i> &nbsp; Username:
-              </div>
-              <input
-                className="usernameInput"
-                name="username"
-                placeholder="Username"
-                onChange={this.handleChange}
-              />
+                <input
+                  className="usernameInput"
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                />
 
-              <div className="email"><i className="fas fa-envelope"></i> &nbsp; Email:
-              </div>
-              <input
-                className="emailInput"
-                name="email"
-                placeholder="Email"
-                onChange={this.handleChange}
-              />
+                <div className="email"><i className="fas fa-envelope"></i> &nbsp; Email:
+                </div>
+                <input
+                  className="emailInput"
+                  name="email"
+                  placeholder="Email"
+                  onChange={this.handleChange}
+                />
 
-              <div className="password"><i className="fas fa-lock"></i> &nbsp; Password:
-              </div>
-              <input
-                className="passInput"
-                name="password"
-                type="password"
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
+                <div className="password"><i className="fas fa-lock"></i> &nbsp; Password:
+                </div>
+                <input
+                  className="passInput"
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
 
-              <div className="password-confirmation"><i className="fas fa-lock"></i> &nbsp; Confirm Password:
+                <div className="password-confirmation"><i className="fas fa-lock"></i> &nbsp; Confirm Password:
+                </div>
+                <input
+                  className="passconfirmInput"
+                  name="password_confirmation"
+                  type="password"
+                  placeholder="Confirm Password"
+                  onChange={this.handleChange}
+                />
+                <div className="submitButton">
+                  <button className="registerButtonClick">Submit</button>
+                </div>
+                <p> Already signed up? Click <Link className="loginLink" to='/login'>here</Link> to login! </p>
               </div>
-              <input
-                className="passconfirmInput"
-                name="password_confirmation"
-                type="password"
-                placeholder="Confirm Password"
-                onChange={this.handleChange}
-              />
-              <span>
 
-              </span>
-              <div className="submitButton">
-                <button className="registerButtonClick">Submit</button>
-              </div>
-              <p> Already signed up? Click <Link className="loginLink" to='/login'>here</Link> to login! </p>
             </div>
           </form>
         </main>
