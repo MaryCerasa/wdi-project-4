cce72ca040f02d4f174ecb053b3283b1f70245d7
Buse Uslu <buseuslu24@gmail.com>
2019-04-21 20:45:19 +0100

    corrected localhost port

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     historyApiFallback: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:4000',
         secure: false
       }
     }
